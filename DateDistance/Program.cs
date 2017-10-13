f8f9115566185d94316ade7beec8f79b1dd07200
Pierce Lauring <pclauring@gmail.com>
2017-10-12 21:02:07 -0400

    Changed some console.write functions for clarity

--- DateDistance/Program.cs
+++ DateDistance/Program.cs
@@ -7,6 +7,7 @@ namespace DateDistance
         static void Main(string[] args)
         {
             DateTime firstDate, secondDate;
+            //try catch to handle user entering an incorrectly formatted DateTime
             try
             {
                 //Entry of the two dates from the user - can also accept time input from user
@@ -16,12 +17,11 @@ namespace DateDistance
                 secondDate = Convert.ToDateTime(Console.ReadLine());
 
                 //Visual confirmation of correct dates entered
-                Console.WriteLine(firstDate);
-                Console.WriteLine(secondDate);
+                Console.WriteLine("The first entered time is: {0}", firstDate);
+                Console.WriteLine("The second entered time is: {0}", secondDate);
 
                 //calculates the time in days minutes hours between dates
                 TimeSpan diff = secondDate - firstDate;
-                Console.WriteLine(diff);
                 Console.WriteLine("The difference between the two dates is {0} days, {1} hours, and {2} minutes.", diff.Days, diff.Hours, diff.Minutes);
             }
             catch (System.FormatException)
